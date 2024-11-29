04dc8c568554fdac51292f2148947995d1c03dc9
Gabriel-hp <gdias6187@gmail.com>
2024-11-28 20:34:20 -0400

    resolvido
    Muito mais simples do que imaginava, basta multipilcar o valor em horas por 60, que o resultado será os minutos

---
+++ ConvHora.js
@@ -0,0 +1,10 @@
+//Crie um programa que converte um valor de tempo dado em horas para minutos. 
+
+function ConvHora(){
+    const Hora = 12;
+
+    const convs = Hora * 60
+
+    console.log ("suas horas são " + convs, "minutos")
+}
+ConvHora()
